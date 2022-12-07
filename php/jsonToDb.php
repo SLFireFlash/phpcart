93f5cff62bb796b37d40021f4a137b6fb71d8482
sl fireflash <sllahiruprasanna@gmail.com>
2022-12-07 18:55:01 +0530

    json data to databse

---
+++ php/jsonToDb.php
@@ -0,0 +1,26 @@
+<?php
+  include "dbconn.php";
+  for ($x = 10; $x <= 20; $x++) {
+    $jsondata = file_get_contents('https://dummyjson.com/products/2'.$x);
+    $data = json_decode($jsondata, true);
+  
+    $title =$data['title'];
+    $dep =$data['description'];
+    $price =$data['price'];
+    $thumbnail =$data['thumbnail'];
+  
+  
+      //insert into mysql table
+      $sql = "INSERT INTO phones (title, dep, price,thumbnail)
+      VALUES('$title', '$dep', '$price', '$thumbnail')";
+      if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+      } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+    
+  }
+
+
+
+?>
